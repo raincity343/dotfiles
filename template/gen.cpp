d138aaef9856e0df0710477eaa45ff9436c8b2c0
raincity <raincity343@outlook.com>
2025-11-24 17:00:28 +0800

    a

--- template/gen.cpp
+++ template/gen.cpp
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define rep(i, a, b) for(int i = (a); i <= (b); ++i)
+#define rep(i, a, b) for (int i = (a); i <= (b); ++i)
 #define sz(x) (int)(x).size()
 using ll = long long;
 
 mt19937_64 rng(chrono::system_clock::now().time_since_epoch().count());
 ll rand(ll l, ll r) { return uniform_int_distribution<ll>(l, r)(rng); }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
   cin.tie(0)->sync_with_stdio(0);
 }
