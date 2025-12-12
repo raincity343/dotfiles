e1c3548ec026a6eac13d7776c893fa897dc14eca
raincity <raincity343@outlook.com>
2025-12-12 15:52:18 +0800

    a

--- template_gen.cpp
+++ template_gen.cpp
@@ -5,9 +5,9 @@ using namespace std;
 #define sz(x) (int)(x).size()
 using ll = long long;
 
-mt19937_64 rng(chrono::system_clock::now().time_since_epoch().count());
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
 ll rand(ll l, ll r) { return uniform_int_distribution<ll>(l, r)(rng); }
 
-int main(int argc, char* argv[]) {
+int main() {
   cin.tie(0)->sync_with_stdio(0);
 }
