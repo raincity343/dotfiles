ae1888bb31c5330189fa81e7087727601cda1912
raincity <raincity343@outlook.com>
2026-01-05 17:54:21 +0800

    a

--- symlink_template.cpp
+++
@@ -1,1 +0,0 @@
-template/sol.cpp
---
+++ template.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for (int i = (a); i <= (b); i++)
+#define sz(x) (int)(x).size()
+using ll = long long;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+}
