29e988a85ebbc651a7a530fb52dce6f279963f5d
raincity <raincity343@outlook.com>
2026-01-22 23:09:03 +0800

    a

---
+++ template/executable_run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+trap "notify-send WA" ERR
+i=1
+make sol
+while [[ -f in$i ]]; do
+    echo $i
+    ./sol < in$i > out$i
+    diff -Zq out$i ans$i
+    ((i++))
+done
+make gen
+make good
+for ((i = 1;; i++)); do
+    echo $i
+    ./gen > in
+    ./sol < in > out
+    ./good < in > ans
+    diff --color=auto -Z out ans
+done
